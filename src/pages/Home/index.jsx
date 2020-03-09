b6baa5f5e79c1488a32b42dd94f673ae743d464b
Mateo Kruk <mkrukuy@gmail.com>
2020-03-09 15:29:12 -0300

    Added title for template pages

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Home = () => {
-  return (
-    <>
-      <h1>Home</h1>
-    </>
-  );
-};
+const Home = () => (
+  <>
+    <section className="hero is-hero-bar">
+      <div className="hero-body">
+        <h1 className="title">Home</h1>
+      </div>
+    </section>
+    <section className="section is-main-section">Home content</section>
+  </>
+);
 
 export default Home;
