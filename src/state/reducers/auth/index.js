cd50fff5901bd53c0a010c5f078d39c18b81b05d
= <=>
2022-01-20 11:22:29 -0400

    Fixes: converting reducers to tool-kit

--- src/state/reducers/auth/index.js
+++ src/state/reducers/auth/index.js
@@ -1,4 +1,4 @@
-import { createReducer } from 'redux-act';
+import { createReducer } from '@reduxjs/toolkit';
 
 import {
   AUTH_SIGN_IN_INIT,
@@ -24,118 +24,121 @@ import {
   AUTH_UPDATE_USER_DATA,
   AUTH_PROVIDER_FAIL,
   AUTH_PROVIDER_INIT,
-  AUTH_PROVIDER_SUCCESS
+  AUTH_PROVIDER_SUCCESS,
 } from 'state/actions/auth';
 
 const initialState = {
   userData: {
     id: null,
-    isAdmin: null
+    isAdmin: null,
   },
   loading: false,
   error: null,
   restoring: false,
   restoringError: null,
   restoredPassword: false,
-  changedPassword: false
+  changedPassword: false,
 };
 
-export const authReducer = createReducer(
-  {
-    [AUTH_SIGN_IN_INIT]: () => ({
+export const authReducer = createReducer(initialState, (builder) => {
+  builder
+    .addCase(AUTH_SIGN_IN_INIT, () => ({
       ...initialState,
       loading: true,
-    }),
-    [AUTH_SIGN_IN_FAIL]: (state, payload) => ({
+    }))
+    .addCase(AUTH_SIGN_IN_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-    [AUTH_FETCH_USER_DATA_INIT]: () => ({
+    }))
+    .addCase(AUTH_FETCH_USER_DATA_INIT, () => ({
       ...initialState,
       loading: true,
-    }),
-    [AUTH_FETCH_USER_DATA_SUCCESS]: (
-      state,
-      { id, isAdmin, email, name, location, logoUrl, createdAt }
-    ) => ({
-      ...state,
-      userData: {
-        id,
-        isAdmin,
-        email,
-        name,
-        location,
-        logoUrl,
-        createdAt,
-      },
-      loading: false,
-      error: null,
-    }),
-    [AUTH_FETCH_USER_DATA_FAIL]: (state, payload) => ({
+    }))
+    .addCase(
+      AUTH_FETCH_USER_DATA_SUCCESS,
+      (state, { id, isAdmin, email, name, location, logoUrl, createdAt }) => ({
+        ...state,
+        userData: {
+          id,
+          isAdmin,
+          email,
+          name,
+          location,
+          logoUrl,
+          createdAt,
+        },
+        loading: false,
+        error: null,
+      })
+    )
+    .addCase(AUTH_FETCH_USER_DATA_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-    [AUTH_LOGOUT_INIT]: () => ({ ...initialState }),
-    [AUTH_LOGOUT_SUCCESS]: (state) => ({ ...state }),
-    [AUTH_RESTORE_SESSION_INIT]: (state) => ({ ...state, restoring: true }),
-    [AUTH_RESTORE_SESSION_SUCCESS]: (state) => ({
+    }))
+    .addCase(AUTH_LOGOUT_INIT, () => ({ ...initialState }))
+    .addCase(AUTH_LOGOUT_SUCCESS, (state) => ({ ...state }))
+    .addCase(AUTH_RESTORE_SESSION_INIT, (state) => ({
+      ...state,
+      restoring: true,
+    }))
+    .addCase(AUTH_RESTORE_SESSION_SUCCESS, (state) => ({
       ...state,
       restoring: false,
       restoringError: null,
-    }),
-    [AUTH_RESTORE_SESSION_FAIL]: (state) => ({
+    }))
+    .addCase(AUTH_RESTORE_SESSION_FAIL, (state) => ({
       ...state,
       restoring: false,
       restoringError: true,
-    }),
-    [AUTH_SET_PASSWORD_INIT]: (state) => ({ ...state, loading: true }),
-    [AUTH_SET_PASSWORD_SUCCESS]: (state) => ({
+    }))
+    .addCase(AUTH_SET_PASSWORD_INIT, (state) => ({ ...state, loading: true }))
+    .addCase(AUTH_SET_PASSWORD_SUCCESS, (state) => ({
       ...state,
       loading: false,
       error: null,
-    }),
-    [AUTH_SET_PASSWORD_FAIL]: (state, payload) => ({
+    }))
+    .addCase(AUTH_SET_PASSWORD_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-    [AUTH_RESET_PASSWORD_INIT]: () => ({
+    }))
+    .addCase(AUTH_RESET_PASSWORD_INIT, () => ({
       ...initialState,
       loading: true,
-    }),
-    [AUTH_RESET_PASSWORD_SUCCESS]: (state) => ({
+    }))
+    .addCase(AUTH_RESET_PASSWORD_SUCCESS, (state) => ({
       ...state,
       loading: false,
       error: null,
       restoredPassword: true,
-    }),
-    [AUTH_RESET_PASSWORD_FAIL]: (state, payload) => ({
+    }))
+    .addCase(AUTH_RESET_PASSWORD_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-    [AUTH_CLEAN_UP]: (state) => ({
+    }))
+    .addCase(AUTH_CLEAN_UP, (state) => ({
       ...state,
       error: null,
       changedPassword: false,
-    }),
-    [AUTH_CHANGE_PASSWORD_INIT]: (state) => ({
+    }))
+    .addCase(AUTH_CHANGE_PASSWORD_INIT, (state) => ({
       ...state,
       loading: true,
-    }),
-    [AUTH_CHANGE_PASSWORD_SUCCESS]: (state) => ({
+    }))
+    .addCase(AUTH_CHANGE_PASSWORD_SUCCESS, (state) => ({
       ...state,
       loading: false,
       changedPassword: true,
-    }),
-    [AUTH_CHANGE_PASSWORD_FAIL]: (state, payload) => ({
+    }))
+    .addCase(AUTH_CHANGE_PASSWORD_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-    [AUTH_UPDATE_USER_DATA]: (state, payload) => ({
+    }))
+    .addCase(AUTH_UPDATE_USER_DATA, (state, payload) => ({
       ...state,
       userData: {
         id: payload.id,
@@ -146,33 +149,31 @@ export const authReducer = createReducer(
         logoUrl: payload.logoUrl || state.userData.logoUrl,
         createdAt: payload.createdAt,
       },
-    }),
-    [AUTH_PROVIDER_INIT]: (state) => ({
+    }))
+    .addCase(AUTH_PROVIDER_INIT, (state) => ({
       ...state,
       loading: true,
-    }),
-    [AUTH_PROVIDER_SUCCESS]: (
-      state,
-      { id, isAdmin, email, name, location, logoUrl, createdAt }
-    ) => ({
-      ...state,
-      userData: {
-        id,
-        isAdmin,
-        email,
-        name,
-        location,
-        logoUrl,
-        createdAt,
-      },
-      error: null,
-      loading: false,
-    }),
-    [AUTH_PROVIDER_FAIL]: (state, payload) => ({
+    }))
+    .addCase(
+      AUTH_PROVIDER_SUCCESS,
+      (state, { id, isAdmin, email, name, location, logoUrl, createdAt }) => ({
+        ...state,
+        userData: {
+          id,
+          isAdmin,
+          email,
+          name,
+          location,
+          logoUrl,
+          createdAt,
+        },
+        error: null,
+        loading: false,
+      })
+    )
+    .addCase(AUTH_PROVIDER_FAIL, (state, payload) => ({
       ...state,
       loading: false,
       error: payload.error,
-    }),
-  },
-  initialState
-);
+    }));
+});
